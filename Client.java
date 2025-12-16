7f8936f42ed28c6fba47fa93d1ddb59ce7571297
Uliana <124802136+UlianaTheBesd@users.noreply.github.com>
2025-12-16 22:59:48 +0300

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,45 @@
+/*
+Класс Client.
+(Клиент в ресторане).
+- имеет уникальный id заказа (Atomic, чтобы не было ошибок ввиду конкуренции) : idOfOrder.
+- каждые 2-7 секунд делает заказ : queue.put(order).
+ */
+
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Client extends Thread {
+
+    private static final AtomicInteger idOfOrder = new AtomicInteger(0);
+
+    private final Random random = new Random();
+    private final BlockingQueue<Order> queue;
+    private final List<TypeOfDish> menu;
+
+
+    public Client(BlockingQueue<Order> queue, List<TypeOfDish> menu) {
+        this.queue = queue;
+        this.menu = menu;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Thread.sleep(2000 + random.nextInt(5000)); // 2–7 сек
+
+                TypeOfDish dish = menu.get(random.nextInt(menu.size()));
+
+                int id = idOfOrder.incrementAndGet();
+                Order order = new Order(id, dish);
+
+                queue.put(order);
+                System.out.println("Client ordered: " + dish.getName() + ". Id: " + order.getId() + ".");
+                Thread.sleep(2); // Для правильного вывода - чтобы Waiter не "брал заказ" раньше.
+            }
+        } catch (InterruptedException e) {
+            interrupt();
+        }
+    }
+}
